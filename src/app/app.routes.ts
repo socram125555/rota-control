d770af7ba8dcf06dec692a37775f4f581fcf6c06
Marcos <marcos1998dev@gmail.com>
2024-10-27 08:45:58 -0300

    implements routes in project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+// app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { HistoricalComponent } from './historical/historical.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: MainComponent },
+  { path: 'historico', component: HistoricalComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/historical/historical.component.ts
+++ src/app/historical/historical.component.ts
@@ -1,17 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { DataService, Member } from '../data.service';
 import { HeaderComponent } from '../header/header.component';
+import { DataService, Member } from '../data.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-main',
+  selector: 'app-historical',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeaderComponent],
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss'],
+  imports: [CommonModule, HeaderComponent, FormsModule],
+  templateUrl: './historical.component.html',
+  styleUrl: './historical.component.scss',
 })
-export class MainComponent implements OnInit {
+export class HistoricalComponent implements OnInit {
   members: Member[] = []; // Move a declaração para fora do ngOnInit
   showForm = false;
   newMember: Member = { name: '', idMember: 0, rec: 0, prison: 0, period: '' }; // Corrigir para usar o tipo Member
