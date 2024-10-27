0033068cb65b357bff64cfcde743de60b0889ad9
Marcos <marcos1998dev@gmail.com>
2024-10-27 09:31:38 -0300

    add page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { HistoricalComponent } from './historical/historical.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: 'home', component: MainComponent },
   { path: 'historico', component: HistoricalComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }, // Rota curinga
 ];
 
 @NgModule({
---
+++ src/app/page-not-found/page-not-found.component.ts
@@ -0,0 +1,12 @@
+// page-not-found.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-not-found',
+  template: `
+    <h1>Página Não Encontrada</h1>
+    <p>Desculpe, a página que você está procurando não existe.</p>
+  `,
+  styleUrls: ['./page-not-found.component.scss'],
+})
+export class PageNotFoundComponent {}
