2316bd524a1922003805f53736e0d22a3cad176d
LucasSilva-dev371 <lucasgameplay261@gmail.com>
2024-10-30 16:08:51 -0300

    alterações

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
-  template: `
-    <h1>Página Não Encontrada</h1>
-    <p>Desculpe, a página que você está procurando não existe.</p>
-  `,
+  templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
 export class PageNotFoundComponent {}
